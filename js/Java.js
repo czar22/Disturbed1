cf44569670421a28ec6b5cd29a8019385670110c
czar22 <32684754+czar22@users.noreply.github.com>
2018-07-29 22:23:04 +0100

    Add files via upload

---
+++ js/Java.js
@@ -0,0 +1,7 @@
+function openphonebar() {
+    document.getElementById("phonebar").style.height = "100%";
+}
+
+function closephonebar() {
+    document.getElementById("phonebar").style.height = "0%";
+}
